e9220a79cbcfe3aee48eb216a70166668ec37c13
Tom Schoolar <tom.schoolar@btinternet.com>
2019-09-29 13:47:41 +0100

    Base Code

---
+++ Selinium WebDriver Test Mode/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selinium_WebDriver_Test_Mode
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public static IWebDriver driverGC;
+
+        [AssemblyInitialize]
+        public static void SetUp(TestContext context)
+        {
+            driverGC = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void TestChromeDriver()
+        {
+            driverGC.Navigate().GoToUrl("http://www.google.com");
+            driverGC.FindElement(By.Id("lst-ib")).SendKeys("LEGO");
+            driverGC.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
+        }
+    }
+}
